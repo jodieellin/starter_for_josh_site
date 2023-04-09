22f959d5b20dd90839264a469c4930df4496c30f
jodieellin <jodieellin@yahoo.com>
2023-04-09 21:55:49 +0800

    Update home-page.php
    week6

--- home-page.php
+++ home-page.php
@@ -15,34 +15,31 @@
     
     
  
-    <section class="container-fluid articlesbg">
+        <section class="container-fluid articlesbg">
             <div class="container">
-                <h3 class="text-center">LATEST ARTICLES</h3>
+                <h3 class="text-center"><?php the_field('titleforarticles'); ?></h3>
                 <div class="row"><!--  a row that gives us access to the BS columns-->
                   <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port1.jpg">
-                        <h4 class="article-title">A PORTRAIT</h4>
-                        <p class="date" >12 December 2030</p>
-                        <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-                        </p>
-  <a class="readmore" href="#">CONTINUE READING</a>
+                  <img class="img-fluid" alt=“portait” src="<?php the_field('articleimage');?>">
+                  <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
+                <p class="date" ><?php the_field('articledate'); ?></p>
+                  <p class="article-p"><?php the_field('articleexcerpt'); ?> </p>
+                  <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
                     </div>
                     <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg">
-                        <h4 class="article-title">A PORTRAIT</h4>
-                        <p class="date" >12 December 2020</p>
-                        <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-                        </p>
-                        <a  class="readmore" href="#">CONTINUE READING</a>
+                    <img class="img-fluid" alt=“portait” src="<?php the_field('articleimagemiddle');?>">
+                    <h4 class="article-title"><?php the_field('articletitlemiddle'); ?></h4>
+                  <p class="date" ><?php the_field('articledatemiddle'); ?></p>
+                    <p class="article-p"><?php the_field('articleexcerptmiddle'); ?> </p>
+                    <a href="<?php the_field('readmore'); ?>" class="readmoremiddle">CONTINUE READING</a>
                     </div>
     
                     <div class="col-md-4 text-center">
-                      <img class="img-fluid" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg">
-                        <h4 class="article-title">A PORTRAIT</h4>
-                        <p class="date" >12 December 2020</p>
-                        <p class="article-p"> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-                        </p>
-  <a class="readmore" href="#">CONTINUE READING</a>
+                    <img class="img-fluid" alt=“portait” src="<?php the_field('articleimagelast');?>">
+                    <h4 class="article-title"><?php the_field('articletitlelast'); ?></h4>
+                  <p class="date" ><?php the_field('articledatelast'); ?></p>
+                    <p class="article-p"><?php the_field('articleexcerptlast'); ?> </p>
+                    <a href="<?php the_field('readmore'); ?>" class="readmorelast">CONTINUE READING</a>
                     </div>
                     </div><!-- row-->
                 </div><!-- container-->
