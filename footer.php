141c4f5508d16101c9cff882ff98e3199b3c7c40
jodieellin <jodieellin@yahoo.com>
2023-04-23 22:32:41 +0800

    Update footer.php
    wk 8

--- footer.php
+++ footer.php
@@ -2,10 +2,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <a class="navbar-brand" href="#"><span class="josh">JOSH </span><span class="orange">WHITKIN</span></a>
+                   <!--  <a class="navbar-brand" href="#"><span class="josh">JOSH </span><span class="orange">WHITKIN</span></a> -->
+                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
                 </div>
                 <div class="col-md-6">
-                    <p>Terms and Conditions</p>
+                    <!-- <p>Terms and Conditions</p> -->
+                    <footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
                 </div>
             </div> <!-- row -->
         </div> <!-- container -->
