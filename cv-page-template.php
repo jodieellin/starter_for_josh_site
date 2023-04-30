396751287150c3251e411e7982947e468b1632b8
jodieellin <jodieellin@yahoo.com>
2023-04-30 22:31:42 +0800

    Update cv-page-template.php
    fix up

--- cv-page-template.php
+++ cv-page-template.php
@@ -2,30 +2,21 @@
 /*
 Template Name: CV
 */
-?>
-<?php require_once('page_header.php'); ?> 
-<div class="container-fluid maincontainer">
+?><?php require_once('page_header.php'); ?><div class="container-fluid maincontainer">
     <div class="container">
         <div class="row">
             <section class="maintext">              
-    <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
-    
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts--><?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             
 <div class="post single-page">
         <h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
 
-        <div class="postcontent">
-            <?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-            <?php the_content(); ?>
+        <div class="postcontent"><?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?><?php the_content(); ?>
         <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
         </div><!--postcontet-->
         
     </div><!--post class-->
-
-   
 <?php endwhile; ?>
-    
     <?php else: ?>
     <div class="post">
         <p>Sorry, no posts found.</p>
@@ -36,5 +27,4 @@ Template Name: CV
            
      </div>
     </div>
-  </div>
-<?php get_footer(); ?>
+  </div><?php get_footer(); ?>
